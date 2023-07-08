9f42f77d684fdfa801fc853739ff4be7f86e942c
Dudolho <eduardobgrasel@gmail.com>
2023-07-07 22:11:33 -0300

    dia 1

--- main.py
+++ main.py
@@ -6,9 +6,22 @@ pygame.init() #iniciar o pygame
 screen = pygame.display.set_mode((800, 400)) #difinir o tamanho da tela 
 pygame.display.set_caption('Corredor') #definir o nome da janela
 clock = pygame.time.Clock() #criar uma instancia de relogio para limitar o fps
+test_font = pygame.font.Font('font/PixelType.ttf', 50) #criar uma fonte (tipo da fone, tamanho da fonte)
 
-test_surface = pygame.Surface((100,200)) #superficie de teste
-test_surface.fill('Red') #preecher a superifice com uma cor
+#test_surface = pygame.Surface((100,200)) #superficie de teste
+#test_surface.fill('Red') #preecher a superifice com uma cor
+
+sky_surface = pygame.image.load('graphics/Sky.png').convert() #carregar uma imagem
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('My game', False, 'Black') #renderizar a fonte (texto, AA, cor)
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha() #carregar a lesma1
+snail_rect = snail_surface.get_rect(midbottom = (600, 300))
+
+player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha() #carregar o player
+player_rect = player_surf.get_rect(midbottom = (80, 300)) #recupera a área do jogador e aplica um retangulo
+
+
 
 while True: #loop principal
     for event in pygame.event.get(): #recuperar um evento
@@ -16,7 +29,17 @@ while True: #loop principal
             pygame.quit()
             exit()
     
-    screen.blit(test_surface, (200, 100)) #colocar o test_superficie no na tela, em tal posição ()
+    screen.blit(sky_surface, (0, 0)) #colocar o test_superficie no na tela, em tal posição ()
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+   
+    snail_rect.left -= 3
+    if snail_rect.right < 0:
+        snail_rect.left = 800
+    screen.blit(snail_surface, (snail_rect))
+    player_rect.right += 1 
+    screen.blit(player_surf, (player_rect))
+
 
     pygame.display.update() #atualizar o display
     clock.tick(60) #limitar o fps
